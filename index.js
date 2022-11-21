76da4a79acb82783d02eef1514681b1e19e94842
Nico <nicolasurbanos10@gmail.com>
2022-11-21 10:33:26 -0500

    Se agregan algunos endpoints de prueba con productos falsos (faker). Se agregan los endpoints de categoies, products y users. Se crea la carpeta para el manejo de las rutas

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('My app')
+const express = require('express');
+const routerApi = require('./routes/index');
+
+const app = express();
+const port = 3000;
+
+routerApi(app);
+
+app.get('/', (req, res) => {
+  res.send('Aquí va a estar la mejor tienda de CR7');
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('New endpoint');
+})
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
